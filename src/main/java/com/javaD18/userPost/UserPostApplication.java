88dfaed41190c1c1dc6c40642b42c97d72f4c15d
Nicholas Brown <119603466+NicholasBrown-01@users.noreply.github.com>
2023-07-27 17:41:02 -0500

    Initial Doc Setup

---
+++ src/main/java/com/javaD18/userPost/UserPostApplication.java
@@ -0,0 +1,13 @@
+package com.javaD18.userPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaD18/userPost/UserPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaD18.userPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
